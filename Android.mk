7d00bc5888ebbeeed37d8816412d456175cdc3b4
Arvin Quilao <arquilao@gmail.com>
2014-08-26 15:48:53 +0800

    Initial push

---
+++ Android.mk
@@ -0,0 +1,70 @@
+#
+# Copyright 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
+ifneq ($(filter h700, $(TARGET_DEVICE)),)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := wlan_persist_symlink
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): WLAN_PERSIST_FILE := /persist/WCNSS_qcom_wlan_factory_nv.bin
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_factory_nv.bin
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(WLAN_PERSIST_FILE)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(WLAN_PERSIST_FILE) $(SYMLINK)
+	$(hide) touch $@
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := wlan_module_symlink
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): WLAN_MODULE := /system/lib/modules/pronto/pronto_wlan.ko
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_SHARED_LIBRARIES)/modules/wlan.ko
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(WLAN_MODULE)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(WLAN_MODULE) $(SYMLINK)
+	$(hide) touch $@
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
--- device_h700.mk
+++ device_h700.mk
@@ -15,7 +15,93 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_KERNEL):kernel \
+    device/cti/h700/twrp.fstab:recovery/root/etc/twrp.fstab
+
+PRODUCT_COPY_FILES += \
+device/cti/h700/rootdir/fstab.qcom:root/fstab.qcom \
+device/cti/h700/rootdir/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
+device/cti/h700/rootdir/init.qcom.rc:root/init.qcom.rc \
+device/cti/h700/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
+device/cti/h700/rootdir/init.qcom.usb.sh:root/init.qcom.usb.sh \
+device/cti/h700/rootdir/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
+device/cti/h700/rootdir/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
+device/cti/h700/rootdir/init.qcom.rc:root/init.qcom.rc \
+device/cti/h700/rootdir/init.qcom.sh:root/init.qcom.sh \
+device/cti/h700/rootdir/init.qcom.ssr.sh:root/init.qcom.ssr.sh \
+device/cti/h700/rootdir/init.qcom.syspart_fixup.sh:root/init.qcom.syspart_fixup.sh \
+device/cti/h700/rootdir/init.target.rc:root/init.target.rc \
+device/cti/h700/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
+device/cti/h700/rootdir/etc/init.qcom.audio.sh:system/etc/init.qcom.audio.sh \
+device/cti/h700/rootdir/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+device/cti/h700/rootdir/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+device/cti/h700/rootdir/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+device/cti/h700/rootdir/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+device/cti/h700/rootdir/etc/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh
+
+# Prebuilt input device calibration files
+PRODUCT_COPY_FILES += \
+device/cti/h700/idc/ft5x0x_ts.idc:system/usr/idc/ft5x0x_ts.idc \
+device/cti/h700/idc/Goodix-TS.idc:system/usr/idc/Goodix-TS.idc \
+device/cti/h700/idc/himax-ts.idc:system/usr/idc/himax-ts.idc \
+device/cti/h700/idc/msg2133.idc:system/usr/idc/msg2133.idc \
+device/cti/h700/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+device/cti/h700/idc/qwerty2.idc:system/usr/idc/qwerty2.idc
+
+PRODUCT_COPY_FILES += \
+device/cti/h700/audio_policy.conf:system/etc/audio_policy.conf \
+
+PRODUCT_COPY_FILES += \
+device/cti/h700/media_codecs.xml:system/etc/media_codecs.xml \
+device/cti/h700/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_COPY_FILES += \
+device/cti/h700/sec_config:system/etc/sec_config \
+device/cti/h700/thermal-engine-8610.conf:system/etc/thermal-engine-8610.conf \
+device/cti/h700/xtwifi.conf:system/etc/xtwifi.conf \
+device/cti/h700/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+#PRODUCT_COPY_FILES += \
+#device/cti/h700/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
+#device/cti/h700/modules/coresight-event.ko:system/lib/modules/coresight-event.ko \
+#device/cti/h700/modules/dma_test.ko:system/lib/modules/dma_test.ko \
+#device/cti/h700/modules/evbug.ko:system/lib/modules/evbug.ko \
+#device/cti/h700/modules/gpio_axis.ko:system/lib/modules/gpio_axis.ko \
+#device/cti/h700/modules/gpio_event.ko:system/lib/modules/gpio_event.ko \
+#device/cti/h700/modules/gpio_input.ko:system/lib/modules/gpio_input.ko \
+#device/cti/h700/modules/gpio_matrix.ko:system/lib/modules/gpio_matrix.ko \
+#device/cti/h700/modules/gpio_output.ko:system/lib/modules/gpio_output.ko \
+#device/cti/h700/modules/mmc_test.ko:system/lib/modules/mmc_test.ko \
+#device/cti/h700/modules/msm-buspm-dev.ko:system/lib/modules/msm-buspm-dev.ko \
+#device/cti/h700/modules/oprofile.ko:system/lib/modules/oprofile.ko \
+#device/cti/h700/modules/qcedev.ko:system/lib/modules/qcedev.ko \
+#device/cti/h700/modules/qcrypto.ko:system/lib/modules/qcrypto.ko \
+#device/cti/h700/modules/radio-iris-transport.ko:system/lib/modules/radio-iris-transport.ko \
+#device/cti/h700/modules/reset_modem.ko:system/lib/modules/reset_modem.ko \
+#device/cti/h700/modules/spidev.ko:system/lib/modules/spidev.ko \
+#device/cti/h700/modules/wlan.ko:system/lib/modules/wlan.ko \
+#device/cti/h700/modules/pronto/pronto_wlan.ko:system/lib/modules/pronto/pronto_wlan.ko
+
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+device/cti/h700/permissions/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+device/cti/h700/permissions/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+device/cti/h700/permissions/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+device/cti/h700/permissions/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+device/cti/h700/permissions/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+device/cti/h700/permissions/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+device/cti/h700/permissions/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+device/cti/h700/permissions/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+device/cti/h700/permissions/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+device/cti/h700/permissions/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+device/cti/h700/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+device/cti/h700/permissions/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+device/cti/h700/permissions/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+device/cti/h700/permissions/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+device/cti/h700/permissions/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+device/cti/h700/permissions/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+device/cti/h700/permissions/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 $(call inherit-product, build/target/product/full.mk)
 
